c06867c6f2d99db35eefd9c5c24e4d01da61a6cb
Денис Крыськов <qauqe@list.ru>
2016-09-09 08:18:20 +0300

    watch worker memory usage

--- loop.sh
+++ loop.sh
@@ -4,14 +4,15 @@
 
 NCAT='ncat --ssl 127.0.0.1 1111'
 
-PID=`ps ax|grep 'nginx: worker'|grep -v grep|cut -d\  -f1`
+PID=`ps ax|grep 'nginx: worker'|grep -v grep`
+PID=`echo $PID|cut -d\  -f1`
 echo "worker PID=$PID"
 
 co=0
 while : ; do
   printf 'GET /hello\r\n' | $NCAT
   printf 'GET /time\r\n' | $NCAT
-  ti=$(echo 0 $(awk '/Pss/ {print "+", $2}' /proc/`cat nginx.pid `/smaps) | bc)
+  ti=$(echo 0 $(awk '/Pss/ {print "+", $2}' /proc/$PID/smaps) | bc)
   printf "%04d %s  " $co $ti
   co=$((co+1))
 done
